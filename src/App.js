afa428f8b2f225cd0b0424132594603b67653eab
Rohit Kotian <rohitkotian07@gmail.com>
2024-08-02 21:19:42 +0530

    Initial Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const inputArray = new Array(9).fill('');
+  const [playArea, setPlayArea] = useState(inputArray);
+  const [chance, setChance] = useState('X');
+  const [winner, setWinner] = useState('');
+
+  function handleClick(idx) {
+    const updatedPlayArea = [...playArea];
+    if (!updatedPlayArea[idx] && !winner) {
+      updatedPlayArea[idx] = chance;
+      checkForWinner(updatedPlayArea);
+      checkIfMatchTied(updatedPlayArea);
+      setPlayArea(updatedPlayArea);
+      setChance((prevChance) => (prevChance === 'X' ? 'O' : 'X'));
+    }
+  }
+
+  function checkIfMatchTied(updatedPlayArea) {
+    const filtered = updatedPlayArea.filter((elem) => elem);
+    if (filtered.length === inputArray.length) {
+      setWinner('Match is Tied');
+    }
+  }
+
+  function checkForWinner(area) {
+    const winnerResult = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let elem of winnerResult) {
+      const [a, b, c] = elem;
+      if (area[a] && area[a] === area[b] && area[a] === area[c]) {
+        setWinner(
+          area[a] === 'X' ? 'Winner is Player 1' : 'Winner is Player 2'
+        );
+        break;
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tic Tac Toe</h1>
+      <div className="playContainer">
+        {playArea.map((elem, index) => (
+          <div
+            onClick={() => handleClick(index)}
+            className="playSquare"
+            key={index}
+          >
+            {elem}
+          </div>
+        ))}
+      </div>
+      <h2>
+        {winner
+          ? `${winner}`
+          : chance === 'X'
+          ? 'Turn : Player 1'
+          : 'Turn : Player 2'}
+      </h2>
+      <button
+        className="resetBtn"
+        onClick={() => {
+          setPlayArea(inputArray);
+          setChance('X');
+          setWinner('');
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
